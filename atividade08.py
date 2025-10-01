c8ea0f0023131443936edec7186ae2af57b5ec1e
otavio <otavio.olivindo@aluno.ce.gov.br>
2025-10-01 14:44:25 -0300

    Add files via upload

---
+++ atividade08.py
@@ -0,0 +1,11 @@
+# Atividade 08
+
+# Peça ao usuário:
+# Nome
+# Ano de nascimento
+# Calcule sua idade aproximada e mostre a mensagem:
+# ___, você tem ___ anos.
+nome = input("digite seu nome ")
+ano = int(input("digite seu ano de nascimento "))
+idade = 2025 - ano
+print(f'{nome}, você tem{idade} anos')
